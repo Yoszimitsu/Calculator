b7b71ce6562bffd6ee4870e4cdc8e5c10d69cac6
yoszimitsu <cotam07@gmail.com>
2021-02-09 19:08:24 +0100

    created CalculatorService class + test

---
+++ src/test/java/CalculatorServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void emptyTest(){
+        assertTrue(true);
+    }
+}
