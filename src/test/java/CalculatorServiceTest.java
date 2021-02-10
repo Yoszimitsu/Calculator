556bbb1b77f3aec854396349d23cd8d60b7959bc
yoszimitsu <cotam07@gmail.com>
2021-02-10 01:48:49 +0100

    rebuild CalculatorServiceTest && added new tests

--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -1,84 +1,80 @@
+import errors.AnalyzeInstructionException;
 import errors.EmptyFileException;
-import errors.UnknownInstructionException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.Assert;
+import org.junit.Test;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
+public class CalculatorServiceTest {
 
-class CalculatorServiceTest {
+    CalculatorService calculatorService = new CalculatorService();
 
-    @Mock
-    CalculatorService calculatorService;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
+    @Test
+    public void executeCalculationTest_file1_withoutPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-1.txt", false);
+        assertEquals(36, result, 0);
     }
 
     @Test
-    void emptyTest() {
-        assertTrue(true);
+    public void executeCalculationTest_file1_withPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-1.txt", true);
+        assertEquals(16, result, 0);
+    }
+
+    @Test    public void executeCalculationTest_file2_withoutPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-2.txt", false);
+        assertEquals(16, result, 0);
     }
 
     @Test
-    void executeCalculationTest_file1() {
-        when(calculatorService.getFile(anyString())).thenReturn((getFile("/src/test/java/resources/test-file-1.txt")));
-
-        var result = calculatorService.calculate();
-
-        assertEquals(36, result);
+    public void executeCalculationTest_file2_withPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-2.txt", true);
+        assertEquals(31, result, 0);
     }
 
     @Test
-    void executeCalculationTest_file2() {
-        when(calculatorService.getFile(anyString())).thenReturn((getFile("/src/test/java/resources/test-file-2.txt")));
-
-        var result = calculatorService.calculate();
-
-        assertEquals(32, result);
+    public void executeCalculationTest_file3_withoutPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-3.txt", false);
+        assertEquals(1, result, 0);
     }
 
     @Test
-    void executeCalculationTest_file3() {
-        when(calculatorService.getFile(anyString())).thenReturn((getFile("/src/test/java/resources/test-file-3.txt")));
+    public void executeCalculationTest_file3_withPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-3.txt", true);
+        assertEquals(1, result, 0);
+    }
 
-        var result = calculatorService.calculate();
+    @Test
+    public void executeCalculationTest_file4_withoutPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-4.txt", false);
+        assertEquals(16.6667, result, 0);
+    }
 
-        assertEquals(1, result);
+    @Test
+    public void executeCalculationTest_file4_withPrecedence() {
+        var result = calculatorService.calculate("./src/test/java/resources/test-file-4.txt", true);
+        assertEquals(36.1111, result, 0);
     }
 
 
     @Test
-    void executeCalculationTest_emptyFileException() {
-        when(calculatorService.getFile(anyString())).thenReturn((getFile("/src/test/java/resources/test-empty-file.txt")));
-        assertThrows(EmptyFileException.class,
-                () -> {
-                    calculatorService.calculate();
-                }
+    public void executeCalculationTest_emptyFileException() {
+        Assert.assertThrows(EmptyFileException.class,
+                () -> calculatorService.calculate("./src/test/java/resources/test-empty-file.txt", false)
         );
     }
 
     @Test
-    void executeCalculationTest_unknownInstructionException() {
-        when(calculatorService.getFile(anyString())).thenReturn((getFile("/src/test/java/resources/unknown-instruction-file.txt")));
-        assertThrows(UnknownInstructionException.class,
-                () -> {
-                    calculatorService.calculate();
-                }
+    public void executeCalculationTest_analyzeInstructionException() {
+        Assert.assertThrows(AnalyzeInstructionException.class,
+                () -> calculatorService.calculate("./src/test/java/resources/test-wrong-instruction-structure-file.txt", false)
         );
     }
 
-
-    private File getFile(String filePath) {
-        return new File(filePath);
+    @Test
+    public void executeCalculationTest_unknownInstructionException() {
+        Assert.assertThrows(AnalyzeInstructionException.class,
+                () -> calculatorService.calculate("./src/test/java/resources/test-unknown-instruction-file.txt", false)
+        );
     }
-
 }
