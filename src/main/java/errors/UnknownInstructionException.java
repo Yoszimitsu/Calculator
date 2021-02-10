fe3563ebd5fd59550977212348170fbcdc520bf1
yoszimitsu <cotam07@gmail.com>
2021-02-10 01:50:20 +0100

    UnknownInstructionException - changing inheritance

--- src/main/java/errors/UnknownInstructionException.java
+++ src/main/java/errors/UnknownInstructionException.java
@@ -1,6 +1,6 @@
 package errors;
 
-public class UnknownInstructionException extends RuntimeException {
+public class UnknownInstructionException extends AnalyzeInstructionException {
 
     public UnknownInstructionException(String message) {
         super(message);
